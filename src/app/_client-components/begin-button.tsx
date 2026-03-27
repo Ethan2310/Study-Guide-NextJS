c25815dd0374fccf57cb9dbf1c0e835e1f7fb770
EthanZulzi <ethan@zulzi.com>
2026-03-27 23:37:38 +0200

    create initial new route study-categories

--- src/app/_client-components/begin-button.tsx
+++ src/app/_client-components/begin-button.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import Button from "@/components/button";
 
 export default function BeginButton() {
+    const router = useRouter();
+
     return (
         <Button
             text="Lets Begin"
             color="black"
             width={150}
             height={50}
-            onClick={() => {}}
+            onClick={() => router.push("/study-categories")}
             onFocus={() => console.log("Button is focused")}
             onBlur={() => console.log("Button is blurred")}
         />
---
+++ src/app/study-categories/page.tsx
@@ -0,0 +1,7 @@
+export default function StudyCategoriesPage() {
+    return (
+        <main className="flex flex-col min-h-screen items-center justify-center">
+            <h1 className="heading">Study Categories</h1>
+        </main>
+    );
+}
